eadc7512c5b0cf86a25fb1df9b43101182f6e117
CDX <cdxofemail@163.com>
2018-03-02 17:10:13 +0800

    更新首页UI

---
+++ src/components/common/ratingStar.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "rating-star"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/common/shoplist.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "shoplist"
+    }
+</script>
+
+<style scoped>
+
+</style>
