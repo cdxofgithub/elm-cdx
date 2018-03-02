f70ce3f6e5e047da324c72067b702be2ac814ff4
CDX <cdxofemail@163.com>
2018-03-02 16:59:20 +0800

    更新首页ui

--- src/config/mUtils.js
+++ src/config/mUtils.js
@@ -1,20 +1,40 @@
 //存储localStorage
 export const setStore = (name, content) => {
-    if (!name) return
-    if (typeof content != 'string') {
-        content = JSON.stringify(content)
-    }
-    window.localStorage.setItem(name, content)
+  if (!name) return
+  if (typeof content != 'string') {
+    content = JSON.stringify(content)
+  }
+  window.localStorage.setItem(name, content)
 }
 
 //获取localStorage
 export const getStore = name => {
-    if (!name) return
-    return window.localStorage.getItem(name)
+  if (!name) return
+  return window.localStorage.getItem(name)
 }
 
 //删除localStorage
 export const removeStore = name => {
-    if (!name) return;
-	window.localStorage.removeItem(name);
+  if (!name) return;
+  window.localStorage.removeItem(name);
 }
+
+export const showBack = callback => {
+  document.addEventListener('scroll', () => {
+    showBackFun()
+  }, false)
+
+  //判断是否达到目标点
+  const showBackFun = () => {
+    if (document.documentElement.scrollTop > 500) {
+      callback(true)
+    } else {
+      callback(false)
+    }
+  }
+}
+
+export const animate = (element, target, duration = 400, mode = 'ease-out', callbackl) => {
+
+}
+
